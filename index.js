f98e5d8035343f95ae1c81ae60ab5cbf95b701ac
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-07 14:30:03 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,107 @@
+var _ = require('lodash');
+var ig = require('instagram-node').instagram();
+
+var globalPickResult = {
+    data: {
+        fields: {
+            'id': 'id',
+            'user.username': 'username',
+            'user.full_name': 'full_name',
+            'tags': 'tags',
+            'location': 'location',
+            'link': 'link',
+            'images.standard_resolution.url': 'media',
+            'caption.text': 'caption',
+            'likes.count': 'likes'
+        }
+    }
+};
+
 module.exports = {
+
+    /**
+     * Return pick result.
+     *
+     * @param output
+     * @param pickTemplate
+     * @returns {*}
+     */
+    pickResult: function (output, pickTemplate) {
+        var result = {};
+        // map template keys
+        _.map(_.keys(pickTemplate), function (templateKey) {
+
+            var oneTemplateObject = pickTemplate[templateKey];
+            var outputKeyValue = _.get(output, templateKey, undefined);
+
+            if (_.isUndefined(outputKeyValue)) {
+
+                return result;
+            }
+            // if template key is object - transform, else just save
+            if (_.isObject(oneTemplateObject)) {
+                // if data is array - map and transform, else once transform
+                if (_.isArray(outputKeyValue)) {
+
+                    result = this._mapPickArrays(outputKeyValue, oneTemplateObject);
+                } else {
+
+                    result[oneTemplateObject.key] = this.pickResult(outputKeyValue, oneTemplateObject.fields);
+                }
+            } else {
+
+                _.set(result, oneTemplateObject, outputKeyValue);
+            }
+        }, this);
+
+        return result;
+    },
+
+    /**
+     * System func for pickResult.
+     *
+     * @param mapValue
+     * @param templateObject
+     * @returns {*}
+     * @private
+     */
+    _mapPickArrays: function (mapValue, templateObject) {
+
+        var arrayResult = [],
+            result = templateObject.key? {} : [];
+
+        _.map(mapValue, function (inOutArrayValue) {
+
+            arrayResult.push(this.pickResult(inOutArrayValue, templateObject.fields));
+        }, this);
+
+        if (templateObject.key) {
+
+            result[templateObject.key] = arrayResult;
+        } else {
+
+            result = arrayResult;
+        }
+
+        return result;
+    },
+
+    /**
+     * Set acess token.
+     *
+     * @param dexter
+     */
+    authParams: function (dexter) {
+
+        if (dexter.environment('instagram_access_token')) {
+
+            ig.use({access_token: dexter.environment('instagram_access_token')});
+        } else {
+
+            this.fail('A [instagram_access_token] environment is Required.');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +109,26 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var tag = step.input('tag').first();
+
+        if (!tag) {
+
+            this.fail('A [tag] is Required.');
+        } else {
+
+            this.authParams(dexter);
+
+            ig.tag_media_recent(tag, function (err, result) {
+
+                if (err) {
+
+                    this.fail(err);
+                } else {
+
+                    this.complete(this.pickResult({data: result}, globalPickResult));
+                }
+            }.bind(this));
+        }
     }
 };
